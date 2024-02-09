a862039b89ea589460c0543cf3d356b6786ea83d
Anton Rychagov <ffhbioifcb@gmail.com>
2024-02-09 20:19:18 +0500

    refactoring

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/blog/",
   plugins: [react()],
-  
+  base: "./",
+  build: {
+    outDir: "./docs",
+  },
+
 })
